2ebd248462fe20d56ec2de8bbfac1dd6bd19ba10
Ayush Walke <ayushwalke1@gmail.com>
2026-07-29 22:17:42 +0530

    feat: implement dashboard quick actions component

---
+++ force-app/main/default/lwc/fsamDashboardQuickActions/fsamDashboardQuickActions.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class FsamDashboardQuickActions extends LightningElement {
+    handleNewServiceRequest(){
+        // navigation will be implemented in later sprints
+    }
+    
+    handleViewAssets(){
+        // navigation will be implemented in later sprints
+    }
+    handleViewInventory(){
+        // navigation will be implemented in later sprints
+    }
+}
