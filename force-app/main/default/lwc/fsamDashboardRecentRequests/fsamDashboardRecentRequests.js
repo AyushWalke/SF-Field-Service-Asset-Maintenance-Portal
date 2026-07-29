9cdc5748d6659b4c8b4dde3ea63060f710f856c2
Ayush Walke <ayushwalke1@gmail.com>
2026-07-29 20:16:20 +0530

    feat: implement dashboard recent requests component

---
+++ force-app/main/default/lwc/fsamDashboardRecentRequests/fsamDashboardRecentRequests.js
@@ -0,0 +1,44 @@
+import { LightningElement, api } from 'lwc';
+
+const COLUMNS = [
+    {
+        label: 'Request Number',
+        fieldName: 'requestNumber',
+        type: 'text'
+    },
+    {
+        label: 'Asset',
+        fieldName: 'assetName',
+        type: 'text'
+    },
+    {
+        label: 'Priority',
+        fieldName: 'priority',
+        type: 'text'
+    },
+    {
+        label: 'Status',
+        fieldName: 'status',
+        type: 'text'
+    },
+    {
+        label: 'Created Date',
+        fieldName: 'createdDate',
+        type: 'date',
+        typeAttributes: {
+            year: 'numeric',
+            month: 'short',
+            day: '2-digit'
+        }
+    }
+];
+
+export default class FsamDashboardRecentRequests extends LightningElement {
+    @api requests = [];
+
+    columns = COLUMNS;
+
+    get hasRequest(){
+        return this.requests?.length > 0;
+    }
+}
