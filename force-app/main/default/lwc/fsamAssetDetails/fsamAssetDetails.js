5af657574cfcd6f55d694bfd4e26323783aa1697
Ayush Walke <ayushwalke1@gmail.com>
2026-07-30 19:53:52 +0530

    fix: fix typo

--- force-app/main/default/lwc/fsamAssetDetails/fsamAssetDetails.js
+++ force-app/main/default/lwc/fsamAssetDetails/fsamAssetDetails.js
@@ -1,9 +1,9 @@
 import { LightningElement, api } from 'lwc';
 
 export default class FsamAssetDetails extends LightningElement {
-    @api assets;
+    @api asset;
 
     get warrantyStatus(){
-        return this.assets?.warrantyActive ? 'Active' : 'Expired';
+        return this.asset?.warrantyActive ? 'Active' : 'Expired';
     }
 }
